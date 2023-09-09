c3b4fdd60a4c714dddb12e8804848622ead6a0f7
laxmikant <laxmikant211@gmail.com>
2023-09-09 12:55:03 +0530

    1st commit

---
+++ TestProject1/src/test/java/JavaProject/Demo/Myclass1.java
@@ -0,0 +1,19 @@
+package JavaProject.Demo;
+
+public class Myclass1 {
+
+	
+	int a=89;
+	int b=765;
+	int c;
+	Myclass1(){
+		
+	}
+	public static void main(String[] args) {
+	
+		Myclass1 ref = new Myclass1();
+		
+    System.out.println(ref.a);
+	}
+
+}
